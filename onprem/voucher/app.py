ee0bdc430485b38638ab55941d8afe77c94efa3d
Brian Walsh <brian@bwalsh.com>
2020-02-03 20:22:56 +0000

    Refactors jwt key paths

--- onprem/voucher/app.py
+++ onprem/voucher/app.py
@@ -12,10 +12,10 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
 app = Flask(__name__)
 
-with open(os.getenv('JWT_PUBLIC_KEY_PATH'), "r") as f:
+with open('/fence/keys/jwt_public_key.pem', "r") as f:
     public_key = f.read()
 
-with open(os.getenv('JWT_PRIVATE_KEY_PATH'), "r") as f:
+with open('/fence/keys/jwt_private_key.pem', "r") as f:
     private_key = f.read()
 
 
@@ -33,24 +33,19 @@ def extract_access_token(r):
             abort(401)
         access_token = authentication[1]
     elif 'access_token' in r.cookies:
-        app.logger.warning('checking for access_token in cookies')
+        app.logger.debug('checking for access_token in cookies')
         access_token = r.cookies.get('access_token')
     if not access_token:
-        app.logger.warning('No access token found in cookie or header')
+        app.logger.debug('No access token found in cookie or header')
         abort(401, "No access token found in cookie or header")
     return access_token
 
 def get_access_token(request):
     # app.logger.warning(request.url)
-    app.logger.warning(request.headers)
+    # app.logger.warning(request.headers)
     # app.logger.warning(request.args)
-    # app.logger.warning('calling fence')
-    # response = requests.get('http://fence-service/user/anyaccess', headers=request.headers)
     url = 'http://arborist-service/auth/mapping'
     response = requests.get(url, headers=request.headers)
-    # app.logger.warning(response.status_code)
-    # app.logger.warning(response.text)
-    # app.logger.warning(response.headers)
 
     if not response.status_code == 200:
         app.logger.warning(f'{url} did not return 200 {response.status_code}')
@@ -79,6 +74,7 @@ def nginx_auth():
     for p in access_token['context']['user']['projects']:
         for role in access_token['context']['user']['projects'][p]:
             roles.append(f'{p}-{role}')
+    app.logger.debug({'user_name': user_name, 'roles': roles})
     encoded_jwt = jwt.encode({'user_name': user_name, 'roles': roles}, private_key, algorithm='RS256').decode('UTF-8')
     my_response = jsonify({'message': 'Authenticated'})
     my_response.headers['Authorization'] = f"Bearer {encoded_jwt}"
@@ -87,7 +83,7 @@ def nginx_auth():
 
 @app.route("/user")
 def user():
-    app.logger.warning(request.url)
+    app.logger.debug(request.url)
     access_token = extract_access_token(request)
     access_token = jwt.decode(access_token, verify=False)
     return jsonify({'message': access_token})
@@ -95,8 +91,8 @@ def user():
 
 @app.route("/remote-user")
 def remote_user():
-    app.logger.warning(request.url)
-    app.logger.warning(request.headers)
+    app.logger.debug(request.url)
+    app.logger.debug(request.headers)
     remote_user = request.headers.get('Remote-User')
     remote_roles = request.headers.get('Remote-Roles')
     return jsonify({'Remote-User': remote_user, 'Remote-Roles': remote_roles})
